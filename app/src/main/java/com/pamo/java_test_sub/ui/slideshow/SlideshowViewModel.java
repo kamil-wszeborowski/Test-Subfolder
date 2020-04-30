05c635d3db38a6d1551922e3618634ddbd56df2f
kamil-wszeborowski <s16215@pjwstk.edu.pl>
2020-04-30 21:11:48 +0200

    JAVA-Test-Commit

---
+++ app/src/main/java/com/pamo/java_test_sub/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.pamo.java_test_sub.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
